3f9fdf4a6f0c3d25290bdcf8ed6c246a70f5e262
Julio Garcia <jugarpeupv@gmail.com>
2024-12-14 17:04:11 +0100

    Remove sources completion in copilot-chat buffer

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -115,6 +115,10 @@ return {
         sources = {},
       })
 
+      cmp.setup.filetype({ "copilot-chat" }, {
+        sources = {},
+      })
+
       local t = function(str)
         return vim.api.nvim_replace_termcodes(str, true, true, true)
       end
