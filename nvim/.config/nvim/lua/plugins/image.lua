3e6a91c81b3d8fa6ddad00ef7657c20d3a7efb94
Julio Garcia <jugarpeupv@gmail.com>
2024-12-14 13:44:38 +0100

    Enable image.nvim on markdown

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -28,7 +28,7 @@ return {
         backend = "kitty",
         integrations = {
           markdown = {
-            enabled = false,
+            enabled = true,
             clear_in_insert_mode = false,
             download_remote_images = true,
             only_render_image_at_cursor = true,
