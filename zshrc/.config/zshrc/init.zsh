8b95efe8c75a5166f7c80f3ca2cb7c8bc10b5a5f
Julio Garcia <jugarpeupv@gmail.com>
2025-01-20 17:59:24 +0100

    Update config

--- zshrc/.config/zshrc/init.zsh
+++ zshrc/.config/zshrc/init.zsh
@@ -4,9 +4,13 @@
 
 [[ -x $HOME/.oh-my-zsh/custom/plugins/fzf-tab ]] || sh -c "$(git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab)"
 
-[[ -x $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode ]] || sh -c "$(git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode)"
+[[ -x $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode ]] || sh -c "$(git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode)"
 
 [[ -x $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]] || sh -c "$(git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting)"
+
+[[ -x $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]] || sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
+
+[[ -x $HOME/.oh-my-zsh/custom/plugins/git-open ]] || sh -c "$(git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open)"
 
 [[ -x $HOME/.oh-my-zsh/custom/plugins/evalcache ]] || sh -c "$(git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache)"
 
